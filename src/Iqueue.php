e1b186b38da581ee79eb8e8dc7bccd05f76373b6
hasnul <hasnul.rsusu@gmail.com>
2018-11-30 19:16:53 +0700

    fix model

--- src/Iqueue.php
+++ src/Iqueue.php
@@ -6,12 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Iqueue extends Model
 {
-	protected $connection = "larasirs_cache";
-
- //    public function __construct()
-	// {
-	// 	parent::__construct();
+  	public function __construct()
+	{
+	 	parent::__construct();
 		
-	// 	$this->connection = config('iqueue.connection');
-	// }
+	 	$this->connection = config('iqueue.connection');
+	}
 }
